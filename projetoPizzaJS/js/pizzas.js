cbf106e417789d0214be217a8da7896ed5add979
Matheus <matheus.uspeu@gmail.com>
2021-08-16 14:56:04 -0300

    test commentary

--- projetoPizzaJS/js/pizzas.js
+++ projetoPizzaJS/js/pizzas.js
@@ -7,4 +7,6 @@ let pizzaJson = [
     {id:5, name:'Marguerita', img:'images/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
     {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
     {id:7, name:'Chocolate', img:'images/pizza7.png', price:22.36, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
+
+    // commentary test
 ];
